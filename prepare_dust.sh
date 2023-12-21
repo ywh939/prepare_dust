b4990ae855b671bfeaeb9c761188b636d93eaad8
ywh939 <1135964990@qq.com>
2023-12-21 20:05:52 +0800

    fix: prepare shell

--- prepare_dust.sh
+++ prepare_dust.sh
@@ -6,7 +6,7 @@ raw_dataset_path=$1
 module_root_path=$2
 operate_mode=$3
 
-echo "raw dataset: $raw_dataset_path, module: $module_root_path"
+echo "raw dataset: $raw_dataset_path, module: $module_root_path, operate mode: $operate_mode"
 
 create_dust_dir() {
     echo "create dust directory"
@@ -21,6 +21,7 @@ create_dust_dir() {
 }
 
 prepare_dust() {
+    echo "start prepare dust"
     python main.py \
         --save_log, \
         --check_label, \
@@ -32,9 +33,9 @@ prepare_dust() {
         --pcd_rect_rotate_angle 56
 }
 
-if [ $operate_mode == "create_dust_dir" ]; then
+if [ $operate_mode = "create_dust_dir" ]; then
     create_dust_dir
-elif [ $operate_mode == "prepare_dust" ]; then
+elif [ $operate_mode = "prepare_dust" ]; then
     prepare_dust
 else
     echo "operate mode error"
